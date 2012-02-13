7b4cb2e3d4b3671c33457a242eb3ed031aacb1a7
Ed Shuler <eshuler@mac.com>
2012-02-13 10:47:54 -0500

    made a root path

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Rms3::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'sidewinders#index'
 
   # See how all your routes lay out with "rake routes"
 
