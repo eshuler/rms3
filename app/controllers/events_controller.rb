78930f16f5a5948d2ac459ced5531a3fe283cb42
Ed Shuler <eshuler@mac.com>
2012-02-14 22:34:59 -0500

    added basic authentication

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+  
+  before_filter :login_required
+  
   # GET /events
   # GET /events.json
   def index
--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module Rms3
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.autoload_paths << "#{Rails.root}/lib"
+    
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
